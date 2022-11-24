b87937727f3d0bfdc5a64e7286db69ec00b9a034
betester <ikramzulkarnaen02@gmail.com>
2022-11-24 23:33:25 +0700

    feat(Navbar) : sign up and logged in handler

--- src/components/Layout/Navbar.tsx
+++ src/components/Layout/Navbar.tsx
@@ -1,9 +1,17 @@
 import { getToken } from "@/service";
 import { useEffect, useState } from "react";
 import tw from "twin.macro";
+import { useRouter } from "next/router";
 
 export const Navbar = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const router = useRouter();
+
+  const onClickLoggedIn = () => {
+    !isLoggedIn ? router.push("/login") : setIsLoggedIn(false);
+  };
+
+  const onClickSignUp = () => router.push("/signup");
 
   useEffect(() => {
     getToken() && setIsLoggedIn(true);
@@ -12,7 +20,10 @@ export const Navbar = () => {
   return (
     <div tw="top-0 p-5 flex justify-between ">
       <h1 css={[tw`text-center text-white font-bold text-3xl`]}>Forum</h1>
-      <button>{isLoggedIn ? "Logout" : "Login"}</button>
+      <div>
+        <button tw="px-5" onClick={onClickLoggedIn}>{isLoggedIn ? "Logout" : "Login"}</button>
+        {!isLoggedIn && <button onClick={onClickSignUp}>Sign Up</button>}
+      </div>
     </div>
   );
 };
