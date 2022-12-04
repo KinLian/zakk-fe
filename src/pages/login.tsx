71968c4877fbdf70ca20499aeac0e88b690019b1
Taufik Pragusga <taufik@pragusga@ui.ac.id>
2022-12-04 18:33:12 +0700

    feat(login): protect login page from authenticated user

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -2,13 +2,34 @@ import { FC } from 'react';
 import { LoginForm } from '@/components/Form';
 import { GetServerSideProps } from 'next';
 import nookies from 'nookies';
+import { api } from '@/libs';
 
 const LoginPage: FC = () => <LoginForm />;
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
   const { zakk } = nookies.get(ctx);
+  let isValidToken = false;
 
-  if (zakk) {
+  if (!zakk) {
+    return {
+      props: {},
+    };
+  }
+
+  await api
+    .get('/users/me', {
+      headers: {
+        Authorization: `Bearer ${zakk}`,
+      },
+    })
+    .then(() => {
+      isValidToken = true;
+    })
+    .catch((err) => {
+      nookies.destroy(ctx, 'zakk');
+    });
+
+  if (isValidToken) {
     return {
       redirect: {
         destination: '/',
